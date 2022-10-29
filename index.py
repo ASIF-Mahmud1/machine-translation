11690dad95a4b3b7c04659f06cc0146c6a115a55
ASIF-Mahmud1 <1421015@iub.edu.bd>
2022-10-29 16:27:22 +0600

    [add] working on mac os

---
+++ index.py
@@ -0,0 +1,6 @@
+# import tensorflow as tf
+# import  tensorflow_text as text
+# import wget
+# print(tf.__version__)
+# print(text.__version__)
+# wget.download('https://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/parallel.zip')
