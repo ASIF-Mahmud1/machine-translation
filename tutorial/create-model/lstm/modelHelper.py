81f5cb0a3553530c9e0b0efff25b9379f657c2f7
ASIF-Mahmud1 <1421015@iub.edu.bd>
2022-12-31 18:57:04 +0600

    [add] get reconstructed model function in another file

--- tutorial/create-model/lstm/modelHelper.py
+++ tutorial/create-model/lstm/modelHelper.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+import pickle
+from tensorflow import keras
+from keras.utils.vis_utils import plot_model
+
 def make_inference_models(encoder_inputs, encoder_states, decoder_inputs,decoder_embedding, decoder_lstm, decoder_dense):
     
         encoder_model = tf.keras.models.Model(encoder_inputs, encoder_states)
@@ -17,4 +21,36 @@ def make_inference_models(encoder_inputs, encoder_states, decoder_inputs,decoder
             [decoder_outputs] + decoder_states)
         
         return encoder_model , decoder_model
+
+
+def get_reconstructed_model(model_path):
+    reconstructed_model = keras.models.load_model(model_path)
+    plot_model(reconstructed_model, to_file='modelsummary.png', show_shapes=True, show_layer_names=True)
+    reconstructed_model.summary()
+
+
+    ## Load Dictionaries and Parameters 
+    path_encoder_parameters= path_encoder_parameters
+    path_encoder_dictionary= path_encoder_dictionary
+    path_decoder_parameters= path_decoder_parameters
+    path_decoder_dictionary= path_decoder_dictionary
+
+    # loading
+    with open(path_encoder_parameters, 'rb') as handle:
+        encoder_parameters = pickle.load(handle)
+
+    # loading
+    with open(path_encoder_dictionary, 'rb') as handle:
+        encoder_dictionary = pickle.load(handle)
+
+    # loading
+    with open(path_decoder_parameters, 'rb') as handle:
+        decoder_parameters= pickle.load(handle)
+
+    # loading
+    with open(path_decoder_dictionary, 'rb') as handle:
+        decoder_dictionary = pickle.load(handle)    
+
+    
+    return reconstructed_model, encoder_parameters ,decoder_parameters , encoder_dictionary , decoder_dictionary
 
